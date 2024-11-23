eb83dd6f645a0ded8ea8514a7ba002db93a46c61
Eric Liang <eliang@nyu.edu>
2024-11-23 18:15:31 -0500

    use flash.nvim over hop

--- lua/plugins/spec1.lua
+++ lua/plugins/spec1.lua
@@ -44,16 +44,18 @@ return {
     version = '^1.0.0', -- optional: only update when a new 1.x version is released
   },
   {
-    "phaazon/hop.nvim",
-    branch = "v2",
-    opts = {
-      keys = "etovxqpdygfblzhckisuran"
-    },
+    "folke/flash.nvim",
+    event = "VeryLazy",
+    ---@type Flash.Config
+    opts = {},
+    -- stylua: ignore
     keys = {
-      {"hl", "<cmd>HopLine<cr>", desc = "Hop Line"},
-      {"hw", "<cmd>HopWord<cr>", desc = "Hop Word"},
-      {"hp", "<cmd>HopPattern<cr>", desc = "Hop Pattern"}
-    }
+      { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+      { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+      { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+      { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+      { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+    },
   },
   {
     "nvim-lualine/lualine.nvim",
@@ -88,5 +90,12 @@ return {
     "lukas-reineke/indent-blankline.nvim",
     main = "ibl",
     opts = {}
-  }
+  },
+  {
+    "nvim-treesitter/nvim-treesitter-context",
+    dependencies = {"nvim-treesitter/nvim-treesitter"},
+    config = function()
+      require("treesitter-context").setup()
+    end
+  },
 }
